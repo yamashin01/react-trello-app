58691ecba7b76eed48c785fbb902b5e58ba412cb
yamada <shinya.yamada@benext-corp.com>
2023-09-10 15:20:43 +0900

    ✨ Set DragDropContext component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <DragDropContext>
+        <Droppable droppableId="droppable">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              <Draggable draggableId="item0" index={0}>
+                {(provided) => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    item0
+                  </div>
+                )}
+              </Draggable>
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  );
 }
 
 export default App;
