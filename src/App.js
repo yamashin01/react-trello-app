900172eb9db7b074b38b51822fd1383688eba1e3
yamada <shinya.yamada@benext-corp.com>
2023-09-13 07:34:06 +0900

    ✨ Add onDragEnd func

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import "./App.css";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { useState } from "react";
 
 function App() {
+  const [items] = useState(["item0", "item1"]);
+  const onDragEnd = (result) => {
+    const remove = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, remove[0]);
+  };
   return (
     <div className="dragDropArea">
-      <DragDropContext>
+      <DragDropContext onDragEnd={onDragEnd}>
         <Droppable droppableId="droppable">
           {(provided) => (
             <div {...provided.droppableProps} ref={provided.innerRef}>
-              <Draggable draggableId="item0" index={0}>
+              <Draggable key={0} draggableId="item0" index={0}>
                 {(provided) => (
                   <div
                     className="item"
@@ -16,10 +22,23 @@ function App() {
                     {...provided.draggableProps}
                     {...provided.dragHandleProps}
                   >
-                    item0
+                    {items[0]}
                   </div>
                 )}
               </Draggable>
+              <Draggable key={1} draggableId="item1" index={1}>
+                {(provided) => (
+                  <div
+                    className="item"
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    {items[1]}
+                  </div>
+                )}
+              </Draggable>
+              {provided.placeholder}
             </div>
           )}
         </Droppable>
